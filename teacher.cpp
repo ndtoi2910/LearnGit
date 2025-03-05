7f8ba8089a4a927817d909dd73086f6c5ce13ee7
TOI-PC\Administrator <nguyendinhtoi2910@gmail.com>
2025-03-05 23:28:24 +0700

    delete all

--- teacher.cpp
+++ teacher.cpp
@@ -6,16 +6,4 @@ public:
     void print(){
         cout << "This is class Teacher!" << endl;
     }
-
-    void hello(){
-        cout << "Hello my class" << endl;
-    }
-
-    void student(){
-        cout << "This is student!" << endl;
-    }
-
-    void func(){
-        cout << "func" << endl;
-    }
 };
